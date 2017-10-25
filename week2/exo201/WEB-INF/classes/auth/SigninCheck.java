cb0dd354bb6488379d3a0d7e731d9c85f7e94f6d
Guillaume <guillaume.dufrene@univ-lille1.fr>
2017-10-25 15:43:50 +0200

    Refine Readme, fix test for web part

--- week2/exo201/WEB-INF/classes/auth/SigninCheck.java
+++ week2/exo201/WEB-INF/classes/auth/SigninCheck.java
@@ -1,8 +1,6 @@
 package auth;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 import static utils.HtmlUtils.asString;
 import static utils.HtmlUtils.getTagAttribute;
 import static utils.HtmlUtils.toNextTag;
@@ -17,6 +15,7 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
+import utils.HtmlUtils;
 import utils.WebTool;
 
 public class SigninCheck {
@@ -25,7 +24,7 @@ public class SigninCheck {
 	
 	@Before
 	public void initWebTool() {
-		web = new WebTool("localhost:8080", "/user-web/");
+		web = new WebTool("localhost:8080", "/exo201/");
 	}
 
 	@Test
@@ -34,14 +33,14 @@ public class SigninCheck {
 		boolean okForm = false;
 		boolean pageReachable = false;
 		boolean contains4Fields = false;
-		List<String> expectedFields = Arrays.asList("firstname", "lastname", "email", "password");
+		List<String> expectedFields = Arrays.asList("firstname", "lastname", "email", "password", "confirmPassword");
 			//  - it is reachable
 		try {
 			String content = asString( web.get("register.jsp") );
 			pageReachable = content.contains("html") && web.code() == 200 ;
 			
 			String action = getTagAttribute( content, "form", "action" );
-			if ( action != null && action.contains("signin") ) okForm = true;
+			if ( action != null && action.contains("register") ) okForm = true;
 			
 			String nextInput = content;
 			ArrayList<String> notFound = new ArrayList<>( expectedFields );
@@ -54,14 +53,14 @@ public class SigninCheck {
 		} catch (IOException ioe ) { }
 		assertTrue( "register page should be reachable", pageReachable );
 			//  - it contains a form
-		assertTrue( "register action form is signin servlet", okForm );
+		assertTrue( "register action form is register servlet", okForm );
 			//  - it contains 4 fields with firstname, lastname, email, password
 		assertTrue( "register contains correct fieldnames", contains4Fields );
 	}
 		
 	@Test
 	public void checkSigninServlet() throws IOException {
-		// 5. There is a "/signin" servlet
+		// 5. There is a "/register" servlet
 		String content = asString( web.get("register.jsp") );
 		assertTrue( content.contains("html") && web.code() == 200 );
 		
@@ -71,10 +70,11 @@ public class SigninCheck {
 		data.put("lastname", "Test");
 		data.put("email", userMail2);
 		data.put("password", "test");
-		InputStream is = web.post("signin", WebTool.encodeMap(data));
+		data.put("confirmPassword", "test");
+		InputStream is = web.post("register", WebTool.encodeMap(data));
 		is.close();
 		assertEquals( 
-			"POST /sigin should accept new user and redirect to a page with HTTP code 302",
+			"POST /register should accept new user and redirect to a page with HTTP code 302",
 			302, web.code()
 		);
 	}
@@ -96,9 +96,10 @@ public class SigninCheck {
 				data.put("lastname", "Test");
 				data.put("email", userMail2);
 				data.put("password", "test");
+				data.put("confirmPassword", "test");
 				data.remove(field);
 				
-				InputStream is = web.post("signin", WebTool.encodeMap(data));
+				InputStream is = web.post("register", WebTool.encodeMap(data));
 				content = asString( is );
 				cb.check(field, web.code(), content, null);
 				
@@ -109,21 +110,88 @@ public class SigninCheck {
 		
 	}
 	
+	private String getDivErrorMessage(String content) {
+		boolean found = false;
+		String tmp = content;
+		do {
+			tmp = HtmlUtils.toNextTag(tmp, "div");
+			if ( tmp == null ) break;
+			String role = HtmlUtils.getTagAttribute(tmp, "div", "role");
+			found = "alert".equals(role);
+		} while ( !found );
+		
+		if ( !found ) return null;
+		
+		int i = tmp.indexOf('>');
+		int j = tmp.indexOf("</div");
+		assertTrue( "Missing </div> ?", j > 0 );
+		String msg = tmp.substring(i+1, j);
+		
+		return msg;
+	}
+	
 	@Test
 	public void checkSigninServlet_checkMissingParam() throws IOException {
 		//  - it refuses any attempt with empty or null field
 		doMissingFieldsRequests( (field, code, content, err) -> {
+			System.out.println("Check " + field);
+			
 			assertEquals("No redirect when field in error",
 				200, code
 			);
+			
+			String msg = getDivErrorMessage(content);
+			assertNotNull( "No div with role=alert found when error occured", msg );
+			msg = msg.toLowerCase();
+			
+			boolean errorMessage = false;
+			switch ( field ) {
+			case "firstname":
+				errorMessage = msg.contains("firstname") || msg.contains("prénom");
+				break;
+			case "lastname":
+				errorMessage = msg.contains("lastname") || msg.contains("nom");
+				break;
+			case "email":
+				errorMessage = msg.contains("mail");
+				break;
+			case "password":
+				errorMessage = msg.contains("password") || msg.contains("mot de passe");
+				break;
+			}
+			
+			if ( !errorMessage ) System.out.println( "\t-- error message was :\n" + msg );
+			assertTrue( "Error message do not indicate field '"+field+"' in error", errorMessage );
 		} );
+		
+		String userMail2 = "u_" + (Math.random()*10000) + "@test.com";
+		HashMap<String, String> data = new HashMap<>(); 
+		data.put("firstname", "Test");
+		data.put("lastname", "Test");
+		data.put("email", userMail2);
+		data.put("password", "test");
+		data.put("confirmPassword", "testError");
+		
+		InputStream is = web.post("register", WebTool.encodeMap(data));
+		String content = asString( is );
+		is.close();
+		boolean ok = web.code() == 200;
+		assertTrue( "Register should refuse when password don't match confirmation", ok );
+		
+		String msg = getDivErrorMessage(content);
+		assertNotNull( "No div with role=alert found when error occured", msg );
+		msg = msg.toLowerCase();
+		
+		boolean errorMessage = msg.contains("confirm");
+		assertTrue( "Error message do not indicate erroneous password confirmation", errorMessage );
+		
 	}
 	
 	@Test
 	public void checkSigninServlet_showErrorParam() throws IOException {
 	//  - it shows error on empty or null field
 		doMissingFieldsRequests( (field, code, content, err) -> {
-			assertEquals(200, code);
+			assertEquals("Redirect detected on missing param " + field, 200, code);
 			int i = content.indexOf("name=\""+field+"\"");
 			if ( i > 0 ) {
 				int j = 0;
@@ -157,9 +225,9 @@ public class SigninCheck {
 		data.put("email", "mark.zuckerberg@facebook.com");
 		data.put("password", "test");
 		
-		InputStream is = web.post("signin", WebTool.encodeMap(data));
+		InputStream is = web.post("register", WebTool.encodeMap(data));
 		is.close();
 		okCheckExists = web.code() == 200;
-		assertTrue("signin refuses existing user", okCheckExists);
+		assertTrue("register refuses existing user", okCheckExists);
 	}
 }

84c71bd20d8360443a44c9322752b7a3dfc32209
Guillaume <guillaume.dufrene@univ-lille1.fr>
2017-10-25 12:34:38 +0200

    Add unit tests, update provided bdd

---
+++ week2/exo201/WEB-INF/classes/TestHelper.java
@@ -0,0 +1,38 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import user.UserDao;
+import user.UserDaoSqlite;
+
+public class TestHelper {
+	
+	private static Connection connection;
+	
+	private static class TestUserDao extends UserDaoSqlite {
+		TestUserDao(String str) throws SQLException {
+			super(str);
+			TestHelper.connection = this.conn;
+		}
+	}
+	
+	
+	private static TestUserDao dao = null;
+	
+	public static UserDao createUserDao() throws SQLException {
+		if ( dao != null ) return dao;
+		
+		dao = new TestUserDao( "../users.db" );
+		return dao;
+	}
+	
+	public static int updateDb(String sql) throws SQLException {
+		return connection
+			.createStatement()
+			.executeUpdate(sql);
+	}
+	
+	public static Connection getConnection() {
+		return connection;
+	}
+	
+}
